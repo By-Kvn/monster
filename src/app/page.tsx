0c848024878882dae95b9890d09d6782318fe68a
By-Kvn <kevin.labatte0@gmail.com>
2024-12-16 23:06:03 +0100

    feat(*): add copyrigth mention

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-Roxborough">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-Roxborough relative">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-lime-500 bg-opacity-5">
         <div className=" relative">
           <p className="font-HelveticaNeue absolute top-8 left-5 text-normalText font-light">Just a</p>
@@ -10,6 +11,11 @@ export default function Home() {
           <p className="font-HelveticaNeue absolute right-52 bottom-2 text-normalText font-light">story</p>
         </div>
       </main>
+      <Link href={'#'}>
+        <p className="absolute -right-12 bottom-36 -rotate-90 font-HelveticaNeue text-base font-light opacity-80 ">Monster- Copyrights</p>
+      </Link>
+
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
