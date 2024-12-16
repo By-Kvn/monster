a42ccb4ed061d7761eaf662e2a0c2af7d9715abf
By-Kvn <kevin.labatte0@gmail.com>
2024-12-16 23:13:47 +0100

    feat(*): progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-Roxborough relative">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-lime-500 bg-opacity-5">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-lime-500 bg-opacity-5 z-10">
         <div className=" relative">
           <p className="font-HelveticaNeue absolute top-8 left-5 text-normalText font-light">Just a</p>
           <h1 className="text-myPink font-Roxborough font-black text-[9.375rem] italic">Monster</h1>
           <p className="font-HelveticaNeue absolute right-52 bottom-2 text-normalText font-light">story</p>
         </div>
       </main>
+      <Image
+        src="/monster-5-page-0001.webp"
+        alt="Monster"
+        className="absolute right-48"
+        width={507}
+        height={679}
+      />
       <Link href={'#'}>
         <p className="absolute -right-12 bottom-36 -rotate-90 font-HelveticaNeue text-base font-light opacity-80 ">Monster- Copyrights</p>
       </Link>
